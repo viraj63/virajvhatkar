2dbb298f7abdecb3fbe3a1c60ac554dfd913f073
viraj63 <virajvhatkar6@gmail.com>
2023-06-26 12:42:54 -0700

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; /*
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'; /*
 If you have multiple pages you need to use the above line react-router-dom
 */
 import React from 'react';
