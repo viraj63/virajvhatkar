12b076277f4ed63f6d1bc7a0535a09b96c7f3d03
viraj63 <virajvhatkar6@gmail.com>
2023-06-26 16:38:56 -0700

    Deployed website

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import "../Navbar/Navbar.css";
 import ReorderIcon from '@mui/icons-material/Reorder';
 
-import Button from '@mui/material/Button';  
-import Stack from '@mui/material/Stack';  
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 
 
 function Navbar() {
     const [expandNavbar, setExpandNavbar] = useState(false);
-   
+
     const location = useLocation();
 
     useEffect(() => {
@@ -27,36 +27,30 @@ function Navbar() {
                 </button>
             </div>
 
-    
-
-
-                <div className='links'>
-                    <Link to="/" className="highlight">HOME</Link>
-                    <Link to="/about" className="highlight">ABOUT</Link>
-                    <Link to="/projects" className="highlight">PROJECTS</Link>
-                    <Link to="/experience" className="highlight">EXPERIENCE</Link>
-                    <div className='resume'>
-
-<a href={require("../Navbar/re.pdf")} download="Resume_Viraj_Vhatkar"><Button>   Download_Resume  </Button></a>
 
 
 
-                    </div>
-                    
+            <div className='links'>
+                <Link to="/" className="highlight">HOME</Link>
+                <Link to="/about" className="highlight">ABOUT</Link>
+                <Link to="/projects" className="highlight">PROJECTS</Link>
+                <Link to="/experience" className="highlight">EXPERIENCE</Link>
+                <div className='resume'>
+
+                    <a href={require("../Navbar/re.pdf")} download="Resume_Viraj_Vhatkar"><Button>   Download_Resume  </Button></a>
+
+
+
                 </div>
-{/*                 <div className='resume'>
+
+            </div>
+            {/*                 <div className='resume'>
 
                     <a href={require("../Navbar/re.pdf")} download="Resume_Viraj_Vhatkar"><Button>   Download_Resume  </Button></a>
 
 
 
                 </div> */}
-
-
-
-          
-
-
         </div>
 
     );
